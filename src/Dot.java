a61dca9fc8fc0de4b5b6ceaca9b3896000309e48
alaneric1618 <alaneric1618@gmail.com>
2014-10-12 14:14:40 -0500

    Can now eat dots

--- src/Dot.java
+++ src/Dot.java
@@ -1,30 +1,21 @@
+import java.awt.Rectangle;
 import java.awt.Graphics2D;
 
 public class Dot extends GameObject {
-	
-    boolean exists = true;
-    
-    public static int incrementPoints()
-    {  
-    	int points = 0;
-    	points++;
-    	return points;
+
+    public Dot(int gridX, int gridY) {
+	super();
+	this.boundingBox = new Rectangle(12*gridX+4, 12*gridY+4, 4, 4);
     }
-    
-    public boolean doesExist() {
-	return exists;
-    }
-	
-    public void consume() {
-	exists = false;
-    }
-	
+
+
     public void update(float dt) {
 
     }
 
     public void draw(Graphics2D g) {
-
+	drawSprite(g, 12, 17, 0, -4, -4);
+	super.draw(g);
     }
 
 }
--- src/Pacman.java
+++ src/Pacman.java
@@ -7,27 +7,68 @@ import java.awt.geom.AffineTransform;
 
 public class Pacman extends GameObject implements KeyListener {
 
+    Room room;
     double totalTime = 0;
     double elapsedChompTime = 0;
+    double elapsedDirectionTime = 0;
     int spriteIOffset = 0;
     int velX = 0;
     int velY = 0;
+    int desiredVelX = 0;
+    int desiredVelY = 0;
+    boolean moving = false;
 
-    public Pacman() {
+    public Pacman(Room room) {
 	super();
-	this.boundingBox = new Rectangle((13*12)+8, 26*12+1, 8, 8);
+	this.boundingBox = new Rectangle((13*12)+8, 26*12+1, 11, 11);
+	this.room = room;
     }
 
     public void update(float dt) {
 	double x = boundingBox.getX();
 	double y = boundingBox.getY();
 	elapsedChompTime += dt;
-	boundingBox.setLocation( (int)(x+velX*2), (int)(y+velY*2) );
-	if (elapsedChompTime > 200) {
+	elapsedDirectionTime += dt;
+	//Move to next location
+	Rectangle nextLocation = new Rectangle(boundingBox);
+	Rectangle nextDesiredLocation = new Rectangle(boundingBox);
+	nextLocation.setLocation( (int)(x+velX*2), (int)(y+velY*2) );
+	nextDesiredLocation.setLocation( (int)(x+desiredVelX*2), (int)(y+desiredVelY*2) );
+	if (room.isLocationFree(nextDesiredLocation)) {
+	    elapsedDirectionTime = 0;
+	    moving = true;
+	    velX = desiredVelX;
+	    velY = desiredVelY;
+	    boundingBox.setLocation( (int)(x+velX*2), (int)(y+velY*2) );
+	} else if(room.isLocationFree(nextLocation)) {
+	    moving = true;
+	    elapsedDirectionTime = 0;
+	    boundingBox.setLocation( (int)(x+velX*2), (int)(y+velY*2) );
+	} else {
+	    moving = false;
+	}
+	//Screen Wrap
+	if (x > PacmanGame.WIDTH) {
+	    boundingBox.setLocation( -10, (int)y );
+	}
+	if (x < -10) {
+	    boundingBox.setLocation( PacmanGame.WIDTH, (int)y );
+	}
+	//Reset desired direction
+	if (elapsedDirectionTime > 2500) {
+	    elapsedDirectionTime = 0;
+	    desiredVelX = velX;
+	    desiredVelY = velY;
+	}
+	//Animate chomp
+	if (elapsedChompTime > 200 && moving) {
 	    elapsedChompTime = 0;
 	    if (spriteIOffset == 0) spriteIOffset = 2;
 	    else spriteIOffset = 0;
+	} else if (!moving) {
+	    spriteIOffset = 2;
 	}
+	//Animate direction
 	if (Math.abs(velX) > Math.abs(velY)) { //Going left or right
 	    if (velX > 0) {  //Right
 		spriteI = 12; spriteJ= 7;
@@ -51,27 +92,27 @@ public class Pacman extends GameObject implements KeyListener {
     public void draw(Graphics2D g) {
 	double x = boundingBox.getX();
 	double y = boundingBox.getY();
-	drawSprite(g, 24, spriteI+spriteIOffset,spriteJ, -7, -7);
+	drawSprite(g, 24, spriteI+spriteIOffset,spriteJ, -5, -5);
 	super.draw(g);
     }
 
     public void keyPressed(KeyEvent e) {
 	int code = e.getKeyCode();
 	if (e.VK_LEFT == code) {
-	    velX = -1;
-	    velY = 0;
+	    desiredVelX = -1;
+	    desiredVelY = 0;
 	}
 	if (e.VK_RIGHT == code) {
-	    velX = 1;
-	    velY = 0;
+	    desiredVelX = 1;
+	    desiredVelY = 0;
 	}
 	if (e.VK_UP == code) {
-	    velX = 0;
-	    velY = -1;
+	    desiredVelX = 0;
+	    desiredVelY = -1;
 	}
 	if (e.VK_DOWN == code) {
-	    velX = 0;
-	    velY = 1;
+	    desiredVelX = 0;
+	    desiredVelY = 1;
 	}
     }
 
